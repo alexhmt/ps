0ee85d85f2d20dc90b2bc804a04a5c0537adaa2e
Alexhmt <alexhmt@gmail.com>
2020-10-25 15:22:22 +1000

    1

---
+++ Untitled-1.ps1
@@ -0,0 +1,35 @@
+$d = Import-Csv -Delimiter "," data.cvs
+
+foreach ($item in $d) {
+    Write-Host   $item.Name "------"  $item.Password
+}
+
+
+
+
+
+<#
+param([string]$Username="LocalAdmin",[string]$GroupName="Administrators")
+foreach($Computer in (Get-Content "file.txt"))
+{
+    
+    # echo $Username
+    $ADSI = [ADSI]("WinNT://$Computer")
+
+    #username
+    $NewUser = $ADSI.Create('User',$Username) 
+
+    #input password
+    $Password = Read-Host -Prompt "Enter password for $Username" -AsSecureString
+    $BSTR = [system.runtime.interopservices.marshal]::SecureStringToBSTR($Password)
+    $_password = [system.runtime.interopservices.marshal]::PtrToStringAuto($BSTR)
+    $NewUser.SetPassword(($_password))
+    $NewUser.SetInfo()
+
+    #set group
+    $Group = $ADSI.Children.Find($GroupName, 'group')
+    $Group.Add(("WinNT://$Computer/$Username"))
+    $NewUser.SetInfo()
+
+}
+#>
